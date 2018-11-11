6a85d80fa68c6cacc267dd0838f654009c91e037
233boy <onehack.ttt@gmail.com>
2018-11-11 15:00:38 +0800

    bug fix

--- src/download-caddy.sh
+++ src/download-caddy.sh
@@ -1,6 +1,6 @@
 _download_caddy_file() {
-	local caddy_tmp="/tmp/install_caddy/"
-	local caddy_tmp_file="/tmp/install_caddy/caddy.tar.gz"
+	caddy_tmp="/tmp/install_caddy/"
+	caddy_tmp_file="/tmp/install_caddy/caddy.tar.gz"
 	[[ -d $caddy_tmp ]] && rm -rf $caddy_tmp
 	if [[ $sys_bit == "i386" || $sys_bit == "i686" ]]; then
 		local caddy_download_link="https://caddyserver.com/download/linux/386?license=personal"
--- v2ray.sh
+++ v2ray.sh
@@ -10,7 +10,7 @@ none='\e[0m'
 # Root
 [[ $(id -u) != 0 ]] && echo -e " 哎呀……请使用 ${red}root ${none}用户运行 ${yellow}~(^_^) ${none}" && exit 1
 
-_version="v3.02"
+_version="v3.03"
 
 cmd="apt-get"
 
