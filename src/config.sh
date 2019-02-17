9d6febc8b2a30722a8379cab27eda4d9212ac565
233boy <onehack.ttt@gmail.com>
2019-02-17 23:15:24 +0800

    update

--- src/config.sh
+++ src/config.sh
@@ -1,6 +1,14 @@
 
 # vmess
 _load vmess-config.sh
+# ss
+_load ss-config.sh
+
+# socks
+_load socks-config.sh
+
+# mtproto
+_load mtproto-config.sh
 
 # ban domain
 _load ban_xx.sh
@@ -10,18 +18,6 @@ _load ban_bt.sh
 
 # ban ad
 _load ban_ad.sh
-
-# custom rules
-_load custom_rules.sh
-
-# ss
-_load ss-config.sh
-
-# socks
-_load socks-config.sh
-
-# mtproto
-_load mtproto-config.sh
 
 # custom config
 _load custom_config.sh
