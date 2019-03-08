83479d48dcb906483585d796d550462d7e021ea8
cutelua <33216131+cutelua@users.noreply.github.com>
2019-03-08 13:50:02 +0800

    git clone depth

--- install.sh
+++ install.sh
@@ -755,7 +755,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/233boy/v2ray
 	else
 		pushd /tmp
-		git clone https://github.com/233boy/v2ray -b "$_gitbranch" /etc/v2ray/233boy/v2ray
+		git clone --depth=1 https://github.com/233boy/v2ray -b "$_gitbranch" /etc/v2ray/233boy/v2ray
 		popd
 
 	fi
