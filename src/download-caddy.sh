2ac06f2065c671435cf5da0cd451c376f33f09ba
rory <tonari753@gmail.com>
2019-07-20 11:55:07 +0800

    coady下载

--- src/download-caddy.sh
+++ src/download-caddy.sh
@@ -2,7 +2,7 @@ _download_caddy_file() {
 	caddy_tmp="/tmp/install_caddy/"
 	caddy_tmp_file="/tmp/install_caddy/caddy.tar.gz"
 	[[ -d $caddy_tmp ]] && rm -rf $caddy_tmp
-	local caddy_download_link="https://caddyserver.com/download/linux/${caddy_arch}?license=personal"
+	local caddy_download_link="https://caddyserver.com/download/linux/amd64${caddy_arch}?license=personal"
 
 	mkdir -p $caddy_tmp
 
