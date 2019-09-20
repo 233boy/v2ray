b021408f6b014a09ec82c43ea91bf1a50b8e97ca
fangfang852004 <41331597+fangfang852004@users.noreply.github.com>
2019-09-20 11:04:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -803,7 +803,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/233boy/v2ray
 	else
 		pushd /tmp
-		git clone https://github.com/233boy/v2ray -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
+		git clone https://github.com/fangfang852004/v2ray.git -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
 		popd
 
 	fi
