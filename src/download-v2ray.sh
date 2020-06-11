f946429fe835c87ad7f6dd14cab2ecaf2398184a
233boy <31657650+233boy@users.noreply.github.com>
2020-06-11 18:24:56 +0800

    use v2fly releases

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -1,5 +1,5 @@
 _get_latest_version() {
-	v2ray_repos_url="https://api.github.com/repos/v2ray/v2ray-core/releases/latest?v=$RANDOM"
+	v2ray_repos_url="https://api.github.com/repos/v2fly/v2ray-core/releases/latest?v=$RANDOM"
 	v2ray_latest_ver="$(curl -s $v2ray_repos_url | grep 'tag_name' | cut -d\" -f4)"
 
 	if [[ ! $v2ray_latest_ver ]]; then
@@ -17,7 +17,7 @@ _get_latest_version() {
 _download_v2ray_file() {
 	[[ ! $v2ray_latest_ver ]] && _get_latest_version
 	v2ray_tmp_file="/tmp/v2ray.zip"
-	v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
+	v2ray_download_link="https://github.com/v2fly/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
