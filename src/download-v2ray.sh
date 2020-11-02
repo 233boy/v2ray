877f506f46a83f66be535cfd7f7250e636e81626
Blink <blink1024@foxmail.com>
2020-11-03 00:04:47 +0800

    修改下载地址

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -17,7 +17,8 @@ _get_latest_version() {
 _download_v2ray_file() {
 	[[ ! $v2ray_latest_ver ]] && _get_latest_version
 	v2ray_tmp_file="/tmp/v2ray.zip"
-	v2ray_download_link="https://github.com/v2fly/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
+	#v2ray_download_link="https://github.com/v2fly/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
+	v2ray_download_link="https://gitee.com/blink1024/tmpfile/attach_files/508756/download/v2rui-linux-64.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
