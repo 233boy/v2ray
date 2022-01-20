79be7a13d06c08d4287d3fb9d664d446fff0ea3c
233boy <31657650+233boy@users.noreply.github.com>
2022-01-20 20:59:16 +0800

    fix #822

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -51,8 +51,8 @@ Type=simple
 User=root
 #User=nobody
 Environment="V2RAY_VMESS_AEAD_FORCED=false"
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/bin/env v2ray.vmess.aead.forced=false /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json
 #Restart=on-failure
