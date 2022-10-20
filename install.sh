12f84cf6974ae30fde387609ea7701c36f584ea9
TelksBr <77636550+TelksBr@users.noreply.github.com>
2022-10-20 04:35:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -802,7 +802,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/233boy/v2ray
 	else
 		pushd /tmp
-		git clone https://github.com/TelksBr/v2ray/tree/master -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
+		git clone https://github.com/TelksBr/v2ray.git -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
 		popd
 
 	fi
