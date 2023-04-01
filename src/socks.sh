7b4e2f98b3f5e08d48b1daca71245476fc58a90d
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2023-04-01 09:25:44 +0800

    Update socks.sh

--- src/socks.sh
+++ src/socks.sh
@@ -207,7 +207,7 @@ socks_user_config() {
 	echo
 	while :; do
 		#read -p "$(echo -e "请输入$yellow用户名$none...(默认用户名: ${cyan}233blog$none)"): " new_socks_username
-		new_socks_username=dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev
+		new_socks_username=$(echo "$RANDOM" | md5sum | cut -b 1-8)
 		#new_socks_username="aa321321"
 		case $new_socks_username in
 		*[/$]* | *\&*)
@@ -233,7 +233,7 @@ socks_pass_config() {
 	while :; do
 		#read -p "$(echo -e "请输入$yellow密码$none...(默认密码: ${cyan}233blog.com$none)"): " new_socks_userpass
 		#new_socks_userpass="aa321321"
-		new_socks_userpass=dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev
+		new_socks_userpass=$(echo "$RANDOM" | md5sum | cut -b 1-8)
 		case $new_socks_userpass in
 		*[/$]* | *\&*)
 			echo
