d9997054795ed2b4141fae9de276e35fb1f4783a
233boy <31657650+233boy@users.noreply.github.com>
2024-05-08 20:02:55 +0800

    add dns set

--- src/core.sh
+++ src/core.sh
@@ -1700,7 +1700,7 @@ is_main_menu() {
         show_help
         ;;
     9)
-        ask list is_do_other "启用BBR 查看日志 查看错误日志 测试运行 重装脚本"
+        ask list is_do_other "启用BBR 查看日志 查看错误日志 测试运行 重装脚本 设置DNS"
         case $REPLY in
         1)
             load bbr.sh
@@ -1717,6 +1717,10 @@ is_main_menu() {
             ;;
         5)
             get reinstall
+            ;;
+        6)
+            load dns.sh
+            dns_set
             ;;
         esac
         ;;
@@ -1794,6 +1798,10 @@ main() {
         is_dont_auto_exit=
         [[ $is_api_fail ]] && manage restart &
         [[ $is_del_host ]] && manage restart caddy &
+        ;;
+    dns)
+        load dns.sh
+        dns_set ${@:2}
         ;;
     debug)
         is_debug=1
